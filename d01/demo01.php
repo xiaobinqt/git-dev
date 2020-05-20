21bf71b2549cf32372f889d3708caf00b05fd83e
v_bivwei <v_bivwei@tencent.com>
2020-05-20 23:00:55 +0800

    dev_01

---
+++ d01/demo01.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: v_bivwei
+ * Date: 2020/5/20
+ * Time: 22:59
+ */
+
+
+$post_data['opinion'] = 2;
+$opinion = null;
+if ($opinion) $post_data['opinion'] = $opinion;
+var_dump($opinion);
